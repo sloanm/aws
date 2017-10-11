90f22e381ec3dc35adfaec388c165ad2801d8788
SLOAN MILLER <sloanmiller@MacBookPro.local>
2017-10-11 12:09:59 -0700

    Install nginx

---
+++ default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/www/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
